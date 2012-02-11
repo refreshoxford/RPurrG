3aa28c9c7462ca1612a9030f882ad6b309080765
Perry Roper <perryroper@gmail.com>
2012-02-11 14:51:16 +0000

    Add compass and initial cat selection

--- js/script.js
+++ js/script.js
@@ -1,8 +1,46 @@
-/* Author:
+$(function() {
 
-*/
+  var messages = $('#messages');
+ 
+  $('#cat-selection .cat').on('click', function() {
+  
+    //Set the selected cat variable
+    selectedCat = $(this).attr('id');
 
+    //Check for a username
+    if($('#player-name').val() != ''){
+      $('#start-game').addClass('active');
+    } 
 
+    messages.html('You have selected ' + selectedCat);
 
+  });
 
+  $('#start-game').on('click', function(e) {
 
+    e.preventDefault();
+
+    if($(this).hasClass('active')) {
+
+      var playerName = $('#player-name').val();
+      if(selectedCat && playerName) {
+        console.log(selectedCat)
+
+        //Initialise the game
+        initialise(selectedCat, playerName)
+
+      } else {
+        console.log('You havent selected a cat');
+
+      }
+
+    }
+
+  });
+
+  function initialise(selectedCat, playerName) {
+    messages.html(playerName +', you have selected '+ selectedCat); 
+  
+  }
+
+});
